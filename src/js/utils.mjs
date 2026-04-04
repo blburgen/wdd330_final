8fbbf349a794eb2a37df09779dc32bb9c01f555d
Brady Burgener <bradylbsee@gmail.com>
2026-04-04 07:05:17 -0600

    image home

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -21,3 +21,35 @@ export function setClick(selector, callback) {
   });
   qs(selector).addEventListener("click", callback);
 }
+
+export function getParam(param){
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+  const product = urlParams.get(param)
+  return product;
+}
+
+export function renderWithTemplate(template, parentElement, data, callback){
+  parentElement.innerHTML = template;
+
+  if(callback){
+    callback(data);
+  }
+}
+
+export async function loadTemplate(path){
+  const res = await fetch(path);
+  const template = await res.text();
+  return template;
+}
+
+export async function loadHeaderFooter(){
+  const headerTemplate = await loadTemplate("../public/partials/header.html");
+  const headerElement = document.querySelector("#main-header");
+  renderWithTemplate(headerTemplate, headerElement);
+
+  const footerTemplate = await loadTemplate("../public/partials/footer.html");
+  const footerElement = document.querySelector("#main-footer");
+  renderWithTemplate(footerTemplate, footerElement);
+  updateCartCount();
+}
